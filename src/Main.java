434c2bd4ed17fc421b172747b16331f3546081b8
bremartin95 <breannamartin995@outlook.com>
2022-09-28 20:45:39 -0500

    test commit to github

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+     int [][] cakeArray = {
+             {1, 100, 1000},
+             {4,200,2000},
+             {9,300,3000},
+             {16,400,4000},
+             {25, 500,5000}
+
+     };
+    }
+}
